d019351598709d43cf64567f7f95282d236de978
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-11 06:36:02 +0300

    import component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Editor from './Components/Editor';
 
 function App() {
-  return <div className="App">x</div>;
+  return (
+    <div className="App p-5 w-full min-h-screen bg-brand-dark grid grid-cols-1 place-content-center place-items-center">
+      <Editor />
+    </div>
+  );
 }
 
 export default App;
