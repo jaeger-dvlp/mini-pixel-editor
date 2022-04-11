f7920788e4f4763c4b4df5bdca412abb5fd4b58e
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-11 07:29:31 +0300

    handle status && import component

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { CirclePicker } from 'react-color';
+import Canvas from './Canvas';
 
 export default function Editor() {
   const [canvasWidth, setCanvasWidth] = useState(16);
   const [canvasHeight, setCanvasHeight] = useState(16);
   const [canvasOptions, setCanvasOptions] = useState(true);
   const [pixelColor, setPixelColor] = useState('#2196f3');
+  const [pixelBorder, setPixelBorder] = useState(true);
 
   return (
     <div
       id="editor"
-      className="font-pop gap-5 transition-all duration-150 grid grid-cols-1 place-content-start place-items-start min-w-[16rem] bg-brand-light rounded-xl shadow-xl overflow-hidden p-5"
+      className="font-pop gap-5 transition-all duration-150 grid grid-cols-1 place-content-start place-items-center min-w-[16rem] bg-brand-light rounded-xl shadow-xl overflow-hidden p-5"
     >
       <div className="w-full grid grid-cols-1 place-content-start place-items-start">
         <h1 className="text-zinc-300 text-xl pb-3 border-b-2 w-full mb-3 border-zinc-200/10 font-normal">
@@ -56,6 +58,27 @@ export default function Editor() {
             onChangeComplete={setPixelColor}
           />
         </div>
+      )}
+      {!canvasOptions && (
+        <>
+          <Canvas
+            width={canvasWidth}
+            height={canvasHeight}
+            color={pixelColor.hex || pixelColor}
+            border={pixelBorder}
+          />
+          <div className="flex justify-start gap-1">
+            <input
+              type="checkbox"
+              className="rounded-xl checked:!bg-red-500 bg-zinc-400 border-none"
+              defaultChecked
+              onChange={(e) => setPixelBorder(e.target.checked)}
+            />
+            <span className="text-zinc-400 text-xs font-normal">
+              Activate Borders
+            </span>
+          </div>
+        </>
       )}
       <button
         type="button"
