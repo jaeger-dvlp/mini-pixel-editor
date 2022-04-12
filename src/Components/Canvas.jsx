c020e89df7f16cd5a5a02edcbcdf3ffd3ae06107
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-13 02:15:48 +0300

    convert to canvas && handle canvas click

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,21 +1,50 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import CanvasRow from './CanvasRow';
+import React, { useEffect, useRef } from 'react';
 
 export default function Canvas(props) {
-  const { width, height, color, border } = props;
+  const { width, height, color /* border */ } = props;
 
-  const rows = [];
+  const canvasPalette = useRef();
 
-  for (let x = 0; x < height; x += 1) {
-    rows.push(
-      <CanvasRow key={x} width={width} color={color} border={border} />,
+  const drawPixel = (x, y, ctx) => {
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    ctx.strokeRect(
+      x + 0.5,
+      y + 0.5,
+      canvasPalette.current.width / width,
+      canvasPalette.current.height / height,
     );
-  }
+    ctx.fillRect(
+      x + 0.5,
+      y + 0.5,
+      canvasPalette.current.width / width,
+      canvasPalette.current.height / height,
+    );
+  };
+
+  const handleCanvasClick = (e) => {
+    const rect = canvasPalette.current.getBoundingClientRect();
+    const x = Math.floor(((e.clientX - rect.left) * width) / rect.width);
+    const y = Math.floor(((e.clientY - rect.top) * height) / rect.height);
+    drawPixel(x, y, canvasPalette.current.getContext('2d'));
+  };
+  useEffect(() => {
+    canvasPalette.current.addEventListener('click', handleCanvasClick);
+
+    return () =>
+      canvasPalette.current &&
+      canvasPalette.current.removeEventListener('click', handleCanvasClick);
+  }, [color]);
 
   return (
-    <div className="animate-fade-in p-0 bg-white grid grid-cols-1 place-content-start place-items-start">
-      {rows}
+    <div className="animate-fade-in p-0 grid-cols-1 place-content-center place-items-center">
+      <canvas
+        ref={canvasPalette}
+        className=" bg-white z-1 rounded-lg overflow-hidden pixel-canvas w-[272px] h-[272px]"
+        width={width}
+        height={height}
+      />
     </div>
   );
 }
